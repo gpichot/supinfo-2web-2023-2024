18124079b8c60c772657b9edb4edf87f96e60127
Gabriel Pichot <gabriel.pichot@gmail.com>
2024-04-30 14:58:14 +0200

    react-router dom base

--- jour1/src/components/ProductCard.tsx
+++ jour1/src/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 /**
 .productCard {
@@ -13,8 +14,17 @@ interface ProductCardProps {
 }
 
 export function ProductCard(props: ProductCardProps) {
+  const [quantite, setQuantite] = useState(0);
   const handleClick = () => {
     alert("Ajouté au panier");
+  };
+  const incrementerProduit = () => {
+    setQuantite(quantite + 1);
+  };
+  const decrementerProduit = () => {
+    if (quantite === 0) return;
+
+    setQuantite(quantite - 1);
   };
   return (
     <div className={styles.productCard}>
@@ -22,6 +32,19 @@ export function ProductCard(props: ProductCardProps) {
         {props.name} {props.estEnPromo && "En promo!!!! 🔥"}
       </p>
       <p>Price: {props.price}€</p>
+      <div style={{ display: "flex", flexFlow: "row", gap: 10 }}>
+        <button
+          type="button"
+          onClick={decrementerProduit}
+          disabled={quantite === 0}
+        >
+          -
+        </button>
+        <p>Quantité : {quantite}</p>
+        <button type="button" onClick={incrementerProduit}>
+          +
+        </button>
+      </div>
       <button type="button" onClick={handleClick}>
         Ajouter au panier
       </button>
---
+++ pokedex/src/App.tsx
@@ -0,0 +1,66 @@
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import { PokemonForm } from "./PokemonForm";
+import { PokemonsMocks } from "./pokemons.mocks";
+import { PokemonCard } from "./PokemonCard";
+
+function Menu() {
+  return (
+    <div style={{ display: "flex", flexFlow: "row nowrap", gap: 10 }}>
+      <Link to="/">Accueil</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/pokemons/new">Créer un pokémon</Link>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: (
+      <div>
+        Page non trouvée
+        <br />
+        <a href="/">Retour à l'accueil</a>
+      </div>
+    ),
+    element: (
+      <div>
+        <h1>Pokedex</h1>
+        <Menu />
+        <Outlet />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: (
+          <div>
+            {PokemonsMocks.map((pokemon) => (
+              <PokemonCard pokemon={pokemon} />
+            ))}
+          </div>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <div>Contact</div>,
+      },
+      {
+        path: "/pokemons/new",
+        element: <PokemonForm />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
